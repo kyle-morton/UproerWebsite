41b414f5c4c879a934f4d977d26b99a3baddc2dd
Kyle Morton <kylemortondevelopment@gmail.com>
2017-12-27 17:03:18 -0600

    added in html, css, js from website

---
+++ app/Scripts/uproer.contact.js
@@ -0,0 +1,14 @@
+﻿var uproer = uproer || {};
+uproer.contact = {
+    events: {
+        clear: function () {
+            $('#ContactFormContainer form').find("input[type=text], textarea").val("");
+        }
+    },
+    methods: {
+        init: function () {
+        }
+    }
+}
+
+$(document).ready(uproer.contact.methods.init);
